f3f9546aa245e5a0b22ed44a9aafd06d54bf8060
Dayna-A <dayna.anderson@csu.fullerton.edu>
2019-03-30 20:46:02 -0700

    Variation on master code using an interrupt in lieu of comparing pin states.

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,55 @@
 #include <mbed.h>
 
-// Past a certain point (roughly 110 for my current test)
-// increasing the buffer size ceases to increase the average
-// by a significant value.
-#define BUF_SIZE 1000
-DigitalIn inputPin(PC_0);
+#define BUF_SIZE 2000
+
+void calculateCycle();
+void printAverage();
+
+InterruptIn inputInterrupt(PC_0);
 CircularBuffer<float, BUF_SIZE> buf;
 Serial pc(USBTX, USBRX, "debug", 9600); //TX,RX
+Timer t;
+bool firstWave = 1;
+int changeCount = 0;
 
-void printAverage();
 int main() {
-  // Verify pin availability
-  if (!inputPin.is_connected()) {
-    pc.printf("Error opening I/O for use.");
-    return 1;
-  }
-
-  Timer t;
-  // Avoid floating pin input by setting default to digital low
-  inputPin.mode(PullDown);
-  bool currentState;
-  // lastState is used to detect a change in input state
-  bool lastState = 0;
-  bool firstWave = 1;
-  int count = 0;
+  // Attach the calculatecyle function to the rising 
+  // edge of the interrupt.
+  inputInterrupt.rise(&calculateCycle);
   while (1) {
-    currentState = inputPin.read();
-    // Start/stop the timer on rising edge
-    // on first state-change start timer
-    // on all subsquent state-changes stop the timer,
-    // store the cycle frequency in a circular buffer
-    // reset and restart it to measure the next wave.
-    if (currentState != lastState && currentState == 1) {
-      if (firstWave) {
-        t.start();
-        firstWave = 0;
-      } else {
-        t.stop();
-        buf.push(1 / t.read());
-        t.reset();
-        t.start();
-        // Once the buffer is full, print buffer average.
-        if (++count == BUF_SIZE) {
-          printAverage();
-          count = 0;
-        }
-      }
-    }
-    lastState = currentState;
+
   }
 }
 
+// Called by the interrupt on the rising edge to start 
+// and stop the timer and record frequencies.
+void calculateCycle() {
+  // Start/stop the timer on rising edge
+  // on first state-change start timer
+  // on all subsquent state-changes stop the timer,
+  // store the cycle frequency in a circular buffer
+  // reset and restart it to measure the next wave.
+  if (firstWave) {
+    t.start();
+    firstWave = 0;
+  } else {
+    t.stop();
+    buf.push(1/t.read());
+    t.reset();
+    t.start();
+    // Once the buffer is full, print buffer average.
+    if (++changeCount == BUF_SIZE) {
+      printAverage();
+      changeCount = 0;
+    }
+  }
+}
 // Function to print buffer average.
-void printAverage() {
+void printAverage()
+{
   float sum = 0, val = 0;
-  for (int i = 0; i < BUF_SIZE; i++) {
+  for (int i = 0; i < BUF_SIZE; i++)
+  {
     buf.pop(val);
     sum += val;
   }
