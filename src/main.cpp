b75226326c143adf0b84b08878a2ef9b363f7730
Dayna-A <dayna.anderson@csu.fullerton.edu>
2019-03-30 18:21:42 -0700

    Updated to measure frequency of each square wave cycle.

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,40 @@
 #include <mbed.h>
 
-DigitalIn inputPin(USER_BUTTON); 
-DigitalOut inputIndicatorLED(LED2);
+DigitalIn inputPin(PC_0); 
 Serial pc(USBTX, USBRX, "debug", 9600); //TX,RX
 
 int main() {
   // verify pin availability
-  if (!inputPin.is_connected() || !inputIndicatorLED.is_connected()) {
+  if (!inputPin.is_connected()) {
     pc.printf("Error opening I/O for use.");
     return 1;
   }
-  // ensure LED starts in the off state
-  inputIndicatorLED.write(0);
+
   Timer t;
   // Avoid floating pin input by setting default to digital low
   inputPin.mode(PullDown);
   bool currentState;
-  // recentState is used to detect a change in input state
-  bool recentState=0;
+  // lastState is used to detect a change in input state
+  bool lastState=0;
+  bool firstWave=1;
+
   while (1) {
     currentState=inputPin.read();
-    if (currentState!=recentState){
-      // start the timer on rising edge or
-      // stop and reset timer on falling edge
-      if(currentState) {
-        t.start();
+    // start/stop the timer on rising edge
+    // on first state change start timer
+    // on all subsquent state changes stop the timer,
+    // reset and restart it to measure the next wave.
+    if(currentState!=lastState && currentState==1) {
+      if(firstWave){
+      t.start();
+      firstWave=0;
       }else{
-        t.stop();
-        pc.printf("Pin was on for %.4f seconds\n",t.read());
-        t.reset();
+      t.stop();
+      pc.printf("Frequency: %.4f\n", 1/t.read());
+      t.reset();
+      t.start();
       }
-      recentState=currentState;
     }
-    // turn on/off led to match pin signal
-    inputIndicatorLED = inputPin; 
+    lastState=currentState;
   }
 }
